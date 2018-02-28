37941800e1f8ece60420fcb92a40a366c15484fb
lost273 <dnk273@gmail.com>
2018-02-28 22:26:04 +0300

    28-02

---
+++ EnglishPlease/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EnglishPlease.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
