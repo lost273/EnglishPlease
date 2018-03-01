6d5146b0e170712b4e72cd56122031115cf40f0b
lost273 <dnk273@gmail.com>
2018-03-01 20:55:22 +0300

    01-03

--- EnglishPlease/Startup.cs
+++ EnglishPlease/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace EnglishPlease
 {
