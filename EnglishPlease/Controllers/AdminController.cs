273fee1f6f4301ec0cc18a356f5ab6736ffd76a8
lost273 <dnk273@gmail.com>
2018-03-02 20:23:00 +0300

    02-03

---
+++ EnglishPlease/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EnglishPlease.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishPlease.Controllers
+{
+    public class AdminController : Controller {
+        private UserManager<AppUser> userManager;
+        public AdminController(UserManager<AppUser> usrMgr) {
+            userManager = usrMgr;
+        }
+        public ViewResult Index() {
+            return View(userManager.Users);
+        }
+    }
+}
