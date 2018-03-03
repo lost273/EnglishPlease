e865034b4584564aca14ba92866baf11c0195c7a
lost273 <dnk273@gmail.com>
2018-03-03 21:51:27 +0300

    03-03

--- EnglishPlease/Controllers/AdminController.cs
+++ EnglishPlease/Controllers/AdminController.cs
@@ -13,8 +13,23 @@ namespace EnglishPlease.Controllers
         public AdminController(UserManager<AppUser> usrMgr) {
             userManager = usrMgr;
         }
-        public ViewResult Index() {
-            return View(userManager.Users);
+        public ViewResult Index() => View(userManager.Users);
+        public ViewResult Create() => View();
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateModel model) {
+            if (ModelState.IsValid) {
+                AppUser user = new AppUser { UserName = model.Name, Email = model.Email};
+                IdentityResult result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded) {
+                    return RedirectToAction("Index");
+                }
+                else {
+                    foreach (IdentityError error in result.Errors) {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
